d6089592278f4cc86784939bcd75c1ed27357235
Hiren Patel <ptlhiren535@gmail.com>
2021-07-01 21:20:04 +0530

    Added @CRUD decorator

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { Crud, CrudController } from '@nestjsx/crud';
 
-@Controller('product')
-export class ProductController {}
+import { ProductsService } from './product.service';
+import { Product } from './product~';
+
+@Crud({
+  model: {
+    type: Product,
+  },
+})
+@Controller('products')
+export class ProductsController implements CrudController<Product> {
+  constructor(public service: ProductsService) {}
+}
