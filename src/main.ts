e6d2a54a8a8bbad78521e5676d28b8d83ff425d4
hashptl <ptlhiren535@gmail.com>
2021-07-02 01:05:52 +0530

    Added Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Products')
+    .setDescription('Product Management Service')
+    .setVersion('1')
+    .addTag('products')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
